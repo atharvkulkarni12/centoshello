93a7567d6dc69d10129709562a4e7594a932dca9
atharvkulkarni12 <atharv5199@gmail.com>
2020-09-16 11:36:09 +0000

    new helloworld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,13 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("HelloWorld! in crazy-experiment branch");
-    }
+// Simple Java Program to Print Hello World
+public class HelloWorld {
+
+	public static void main(String[] args)
+	{
+		int i;
+		for(i = 0; i <= 5; i++)
+		{
+			System.out.println("\n Hello World ");
+		}	
+	}
 }
+
